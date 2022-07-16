dea0eb960d534b30bae1a41fcbe0ae512fd53489
GTrunSec <gtrunsec@hardenedlinux.org>
2022-07-15 22:00:36 -0700

    fix overlays

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -86,32 +86,6 @@
     ];
   };
 
-  nixpkgs.overlays = [                          # This overlay will pull the latest version of Discord
-    (self: super: {
-      discord = super.discord.overrideAttrs (
-        _: { src = builtins.fetchTarball {
-          url = "https://discord.com/api/download?platform=linux&format=tar.gz";
-          sha256 = "1bhjalv1c0yxqdra4gr22r31wirykhng0zglaasrxc41n0sjwx0m";
-        };}
-      );
-    })
-    (final: prev:
-     let
-      vscode-insider = prev.vscode.override {
-        isInsiders = true;
-     };
-     in
-    {
-      vscode = vscode-insider.overrideAttrs (oldAttrs: rec {
-       src = (builtins.fetchTarball {
-       url = "https://update.code.visualstudio.com/latest/linux-x64/insider";
-       sha256 = "0dqs12zflrdn9kni5wjag6d3599gnkxhayhcf93pcyw70y3ryi05";
-    });
-      version = "latest";
-    });
-   })
-  ];
-
   services = {
   gnome.gnome-keyring.enable = true;
     pipewire = {                            # Sound
